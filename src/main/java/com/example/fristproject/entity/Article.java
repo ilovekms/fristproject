282e7d6d2e5be9c067b0cd6bee52e0b458b369d0
kms <ilovekms@gmail.com>
2023-07-25 13:02:17 +0900

    커밋

--- src/main/java/com/example/fristproject/entity/Article.java
+++ src/main/java/com/example/fristproject/entity/Article.java
@@ -1,10 +1,7 @@
 package com.example.fristproject.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,8 @@ import lombok.ToString;
 @Getter // 롬복으로 게터 추가
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 전략
+    //@GeneratedValue
     private Long id;
     @Column
     private String title;
