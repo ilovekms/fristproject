66bfad75c3548be73c9338a64a52cf6552689e74
kms <ilovekms@gmail.com>
2023-07-25 12:57:02 +0900

    커밋

--- src/main/java/com/example/fristproject/controller/ArticleController.java
+++ src/main/java/com/example/fristproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -68,5 +69,40 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         // 뷰 페이지 설정
         return "articles/edit";
+    }
+
+   @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+        // 1: DTO를 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        // 2: 엔티티를 DB로 저장
+        // 2-1: DB에서 기존 데이터를 가져옴
+        Article target = articleRepository.findById(articleEntity.getId())
+                .orElse(null);
+        // 2-2: 기존 데이터가 있다면, 값을 갱신
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+        // 3: 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id,
+                         RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+        // 1: 삭제 대상을 가져옴
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2: 대상을 삭제
+        if (target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제가 완료되었습니다.");
+        }
+        // 3: 결과 페이지로 리다이렉트
+        return "redirect:/articles";
     }
 }
