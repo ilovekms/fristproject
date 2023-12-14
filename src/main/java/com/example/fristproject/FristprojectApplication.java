9f50274e61b177d4a056a4049a21a0c239deddd7
kms <ilovekms@gmail.com>
2023-12-14 14:50:38 +0900

    커밋

--- src/main/java/com/example/fristproject/FristprojectApplication.java
+++ src/main/java/com/example/fristproject/FristprojectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FristprojectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FristprojectApplication.class, args);
 	}
