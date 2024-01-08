52f4c0818373f6e4500ce62cc2cb5fde71e6a0c0
kms <ilovekms@gmail.com>
2024-01-08 14:40:36 +0900

    클라우드스터딩

---
+++ src/main/java/com/example/fristproject/ioc/Chicken.java
@@ -0,0 +1,20 @@
+package com.example.fristproject.ioc;/**
+ * packageName    : com.example.fristproject.ioc
+ * fileName       : Chicken
+ * 자바 파일설명 Chicken
+ * <br>
+ * 자바파일 상세설명
+ *
+ * @author 2023년 국가농식품통계서비스 시스템 고도화 개발팀 kms
+ * @since  2024-01-08
+ * @version 1.0
+ * @see
+  * <pre>
+ *  ◈◈◈◈ 개정이력(Modification Information) ◈◈◈◈
+ *
+ *  수정일        수정자            수정내용
+ *  ----------   --------   ---------------------------
+ *   2024-01-08        kms          최초 생성
+ * </pre>
+ */public class Chicken {
+}
